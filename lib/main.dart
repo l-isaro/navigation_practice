13b642644d7ac139f7d1b5cb3f7ebfd7f66f0d34
Duel <139622859+Irenee123@users.noreply.github.com>
2025-02-07 09:29:46 -0800

    Naviagation practice

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,72 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class ProductScreen extends StatelessWidget {
+  const ProductScreen({super.key});
+
+  static final List<Map<String, dynamic>> products = [
+    {
+      'name': 'Pixel',
+      'description': 'Pixel is the most feautureful phone ever',
+      'price': 800,
+      'rating': 2,
+      'color': Colors.blue,
+    },
+    {
+      'name': 'laptop',
+      'description': 'Laptop is the most productive development tool',
+      'price': 2000,
+      'rating': 3,
+      'color': Colors.green,
+    },
+    {
+      'name': 'Tablet',
+      'description': 'Tablet is the most productive development tool',
+      'price': 1500,
+      'rating': 0,
+      'color': Colors.yellow,
+    },
+    {
+      'name': 'Phone',
+      'description': 'Phone is the most productive development tool',
+      'price': 1000,
+      'rating': 0,
+      'color': Colors.red,
+    },
+    {
+      'name': 'Pen Drive',
+      'description': 'Pen Drive is the most productive development tool',
+      'price': 500,
+      'rating': 0,
+      'color': Colors.purple,
+    },
+    {
+      'name': 'Floppy Drive',
+      'description': 'Floppy Drive is the most productive development tool',
+      'price': 50,
+      'rating': 0,
+      'color': Colors.orange,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Product Navigation"), backgroundColor: Colors.blue),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ProductCard(
+            title: products[index]['name'],
+            description: products[index]['description'],
+            price: products[index]['price'],
+            rating: products[index]['rating'],
+            color: products[index]['color'],
+          );
+        },
+      ),
+    );
+  }
+}
